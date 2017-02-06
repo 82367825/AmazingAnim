8cf19b99d8781264f37b096a71ccf254e66b0c4d
linzewu <82367825@qq.com>
2017-02-06 15:02:14 +0800

    Initial commit

---
+++ app/src/main/java/com/zero/amazinganim/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zero.amazinganim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by linzewu on 2017/2/6.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
