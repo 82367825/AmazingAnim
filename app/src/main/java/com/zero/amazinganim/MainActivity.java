fe5b46a09b7aca55dbd36bf6ce83bf8ffd6d108a
linzewu <82367825@qq.com>
2017-02-17 11:47:57 +0800

    commit code

--- app/src/main/java/com/zero/amazinganim/MainActivity.java
+++ app/src/main/java/com/zero/amazinganim/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
         startActivity(new Intent(MainActivity.this, MorphingTickActivity.class));
     }
 }
