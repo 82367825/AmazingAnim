7e1c5b53998060418e3a028b6d8dbad3fbaa542b
linzewu <82367825@qq.com>
2017-02-20 17:45:44 +0800

    commit code

--- app/src/main/java/com/zero/amazinganim/MorphingProgressBarActivity.java
+++ app/src/main/java/com/zero/amazinganim/MorphingProgressBarActivity.java
@@ -1,8 +1,11 @@
 package com.zero.amazinganim;
 
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by linzewu on 2017/2/9.
@@ -10,8 +13,18 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MorphingProgressBarActivity extends AppCompatActivity {
 
+    
+    private ImageView mImageView;
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morphing_progressbar);
+        
+        mImageView = (ImageView) findViewById(R.id.imageview);
+        AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(
+                this, R.drawable.animated_vector_progressbar);
+        mImageView.setImageDrawable(animatedVectorDrawableCompat);   
+        ((Animatable)mImageView.getDrawable()).start();
     }
 }
--- app/src/main/java/com/zero/amazinganim/TransformingPathActivity.java
+++ app/src/main/java/com/zero/amazinganim/TransformingPathActivity.java
@@ -1,6 +1,5 @@
 package com.zero.amazinganim;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
