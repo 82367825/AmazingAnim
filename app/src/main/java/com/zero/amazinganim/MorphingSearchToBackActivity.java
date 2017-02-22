b8610fff324056838c7f5b2565b47858768ed7dd
linzewu <82367825@qq.com>
2017-02-22 21:02:36 +0800

    commit code

---
+++ app/src/main/java/com/zero/amazinganim/MorphingSearchToBackActivity.java
@@ -0,0 +1,17 @@
+package com.zero.amazinganim;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by linzewu on 17-2-22.
+ */
+
+public class MorphingSearchToBackActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
