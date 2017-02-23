d4d7d4f96b943776f698b65417a67f04ebea370a
linzewu <82367825@qq.com>
2017-02-23 18:58:56 +0800

    commit code

--- app/src/main/java/com/zero/amazinganim/MorphingSearchToBackActivity.java
+++ app/src/main/java/com/zero/amazinganim/MorphingSearchToBackActivity.java
@@ -1,18 +1,35 @@
 package com.zero.amazinganim;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by linzewu on 17-2-22.
  */
 
 public class MorphingSearchToBackActivity extends AppCompatActivity {
-
+    
+    private ImageView mImageView;
+    private boolean mIsChecked = false;
+    
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morphing_search_to_back);
+
+        mImageView = (ImageView) findViewById(R.id.imageview);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIsChecked = !mIsChecked;
+                final int[] stateSet = {android.R.attr.state_checked * (mIsChecked ? 1 : -1)};
+                mImageView.setImageState(stateSet, true);
+            }
+        });
     }
+    
 }
