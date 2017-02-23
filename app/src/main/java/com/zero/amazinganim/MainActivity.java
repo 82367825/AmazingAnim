170726f22e16a50de8b8a20a9dacf0a3ea75e154
linzewu <82367825@qq.com>
2017-02-23 09:14:44 +0800

    commit code

--- app/src/main/java/com/zero/amazinganim/MainActivity.java
+++ app/src/main/java/com/zero/amazinganim/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, MorphingFingerActivity.class));
+        startActivity(new Intent(MainActivity.this, MorphingSearchToBackActivity.class));
     }
 }
--- app/src/main/java/com/zero/amazinganim/MorphingSearchToBackActivity.java
+++ app/src/main/java/com/zero/amazinganim/MorphingSearchToBackActivity.java
@@ -13,5 +13,6 @@ public class MorphingSearchToBackActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_morphing_search_to_back);
     }
 }
