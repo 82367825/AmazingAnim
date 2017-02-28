27e2201dac292407174ac50a016f95b6c30f19a4
linzewu <82367825@qq.com>
2017-02-28 10:27:43 +0800

    commit code

--- app/src/main/java/com/zero/amazinganim/MainActivity.java
+++ app/src/main/java/com/zero/amazinganim/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, MorphingMenuActivity.class));
+        startActivity(new Intent(MainActivity.this, MorphingHandwritingActivity.class));
     }
 }
