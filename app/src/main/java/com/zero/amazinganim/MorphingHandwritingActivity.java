d1c533849836fa92ecd29528ff83cac6727ed683
linzewu <82367825@qq.com>
2017-03-05 15:49:00 +0800

    commit code

--- app/src/main/java/com/zero/amazinganim/MorphingHandwritingActivity.java
+++ app/src/main/java/com/zero/amazinganim/MorphingHandwritingActivity.java
@@ -16,20 +16,34 @@ import android.widget.ImageView;
 public class MorphingHandwritingActivity extends AppCompatActivity {
 
     private ImageView mImageView;
+    private ImageView mImageView2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morphing_handwriting);
 
-        mImageView = (ImageView) findViewById(R.id.imageview);
-        AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(
-                this, R.drawable.animated_vector_pathmorphing_handwriting_android);
-        mImageView.setImageDrawable(animatedVectorDrawableCompat);
-        mImageView.setOnClickListener(new View.OnClickListener() {
+//        mImageView = (ImageView) findViewById(R.id.imageview);
+//        AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(
+//                this, R.drawable.animated_vector_pathmorphing_handwriting_android);
+//        mImageView.setImageDrawable(animatedVectorDrawableCompat);
+//        mImageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ((Animatable)mImageView.getDrawable()).start();
+//            }
+//        });
+
+
+        mImageView2 = (ImageView) findViewById(R.id.imageview2);
+        AnimatedVectorDrawableCompat animatedVectorDrawableCompat2 = AnimatedVectorDrawableCompat
+                .create(
+                this, R.drawable.animated_vector_handwriting_linzewu);
+        mImageView2.setImageDrawable(animatedVectorDrawableCompat2);
+        mImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((Animatable)mImageView.getDrawable()).start();
+                ((Animatable)mImageView2.getDrawable()).start();
             }
         });
         
